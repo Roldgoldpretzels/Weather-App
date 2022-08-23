a532f9b1278f9ea51faa809fed73764138c6e6ec
Roldgoldpretzels <100233541+Roldgoldpretzels@users.noreply.github.com>
2022-08-23 19:10:48 -0400

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "eeac12156fmsh027b67dc443ef44p18823fjsn06291d661508",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "501688fb295d6abf3070718f9cdca06e"; 
